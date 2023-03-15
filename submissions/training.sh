69a1e1302fcb4dcdcc1e4a182a8bcf125843901b
Arne Thomsen <63008256+Arne-Thomsen@users.noreply.github.com>
2023-03-15 11:40:16 +0100

    Update training.sh

--- submissions/training.sh
+++ submissions/training.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --constraint=gpu
-#SBATCH --gpus=4
-#SBATCH --qos=regular
-#SBATCH --job-name=training
-#SBATCH --time=08:00:00
 #SBATCH --account=des_g
+#SBATCH --constraint=gpu
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time=08:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
+#SBATCH --job-name=training
 #SBATCH --output=./log_training/training.%j.log
 
 #OpenMP settings:
-export OMP_NUM_THREADS=64
+export OMP_NUM_THREADS=128
+export OMP_PLACES=threads
+export OMP_PROC_BIND=close
 
 #run the application:
-#applications may performance better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun --cpu_bind=cores --gpus 4 --gpu-bind=none python ../deep_lss/apps/run_training.py --tfr_pattern=${1} --net_config=${2} --dir_base=${3}
+srun python ../deep_lss/apps/run_training.py --tfr_pattern=${1} --net_config=${2} --dir_base=${3}
+
+# recommendation from https://my.nersc.gov/script_generator.php. This can be simplified because there is only a single task
+# srun --ntasks=1 --cpus-per-task=128 --cpu_bind=cores --gpus 4 --gpu-bind=single:1 python ../deep_lss/apps/run_training.py --tfr_pattern=${1} --net_config=${2} --dir_base=${3}
