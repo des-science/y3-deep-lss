98762ab714c0787ded416bf0f5028b623d1b0ec3
Arne Thomsen <athomsen@student.ethz.ch>
2023-08-23 06:26:42 -0700

    added multi noise support to the eval script

--- deep_lss/utils/eval.py
+++ deep_lss/utils/eval.py
@@ -37,7 +37,7 @@ def _get_out_file(dir_out, label):
     return os.path.join(dir_out, out_file)
 
 
-def stack_grid_cosmos(tensors, sorted_indices, n_examples_per_cosmo):
+def _stack_grid_cosmos(tensors, sorted_indices, n_examples_per_cosmo):
     """Reshapes the batched evaluations into the correct shape.
 
     Args:
@@ -62,7 +62,7 @@ def stack_grid_cosmos(tensors, sorted_indices, n_examples_per_cosmo):
     return tensors
 
 
-def remove_example_axis(array):
+def _remove_example_axis(array):
     """Takes in a tensor of shape (n_cosmos, n_examples_per_cosmo, None) or (n_cosmos, n_examples_per_cosmo) and checks
     whether the value along axis 1 is constant to remove that redundant axis.
 
@@ -84,7 +84,16 @@ def remove_example_axis(array):
     return array
 
 
-def evaluate_grid(model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, dir_out, file_label=None):
+def evaluate_grid(
+    model,
+    strategy,
+    tfr_pattern,
+    msfm_conf,
+    dlss_conf,
+    net_conf,
+    dir_out,
+    file_label=None,
+):
     """Evaluate the model on the grid part of the CosmoGrid.
 
     Args:
@@ -100,16 +109,25 @@ def evaluate_grid(model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, 
     print("\n")
     LOGGER.info(f"Starting evaluation of the grid")
 
+    dset_kwargs = net_conf["dset"]["eval"]["grid"]
+
+    if "i_noise" in dset_kwargs.keys():
+        is_single_noise = True
+    elif "n_noise" in dset_kwargs.keys():
+        is_single_noise = False
+    else:
+        raise NotImplementedError(f"The network config needs to contain either i_noise or n_noise in dset/eval/grid")
+
     # pipeline constants
     n_cosmos = msfm_conf["analysis"]["grid"]["n_cosmos"]
     n_patches = msfm_conf["analysis"]["n_patches"]
     n_perms_per_cosmo = msfm_conf["analysis"]["grid"]["n_perms_per_cosmo"]
-
-    # TODO multiple shape noise realisations
-    # n_noise_per_example = msfm_conf["analysis"]["grid"]["n_noise_per_example"]
-    # n_examples_per_cosmo = n_patches * n_perms_per_cosmo * n_noise_per_example
-
     n_examples_per_cosmo = n_patches * n_perms_per_cosmo
+
+    # multiple shape and poisson noise realizations
+    if not is_single_noise:
+        n_examples_per_cosmo *= msfm_conf["analysis"]["grid"]["n_noise_per_example"]
+
     n_examples = n_cosmos * n_examples_per_cosmo
     LOGGER.info(f"There's a total of {n_examples} data vectors to be evaluated")
 
@@ -125,12 +143,22 @@ def evaluate_grid(model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, 
 
     # like https://www.tensorflow.org/tutorials/distribute/input#tfdistributestrategydistribute_datasets_from_function
     def dataset_fn(input_context):
-        dset = grid_pipeline.get_dset(
-            tfr_pattern=tfr_pattern,
-            **net_conf["dset"]["eval"]["grid"],
-            # distribution
-            input_context=input_context,
-        )
+        if is_single_noise:
+            dset = grid_pipeline.get_dset(
+                tfr_pattern=tfr_pattern,
+                **dset_kwargs,
+                # distribution
+                input_context=input_context,
+            )
+
+        else:
+            dset = grid_pipeline.get_multi_noise_dset(
+                tfr_pattern=tfr_pattern,
+                **dset_kwargs,
+                # distribution
+                input_context=input_context,
+            )
+
         return dset
 
     dist_dset = strategy.distribute_datasets_from_function(dataset_fn)
@@ -139,8 +167,9 @@ def evaluate_grid(model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, 
 
     preds = []
     cosmos = []
-    sobols = []
-    noises = []
+    i_sobols = []
+    i_noises = []
+    i_examples = []
     for dv_batch, cosmo_batch, index_batch in LOGGER.progressbar(
         dist_dset, at_level="info", total=n_steps, desc="evaluating the grid"
     ):
@@ -154,61 +183,92 @@ def evaluate_grid(model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, 
         # shape (global_batch_size,) NOTE it's important that gather takes place on the tensor (not tuple) level
         i_sobol_batch = strategy.gather(index_batch[0], axis=0)
         i_noise_batch = strategy.gather(index_batch[1], axis=0)
+        i_example_batch = strategy.gather(index_batch[1], axis=0)
 
         preds.append(pred_batch)
         cosmos.append(cosmo_batch)
-        sobols.append(i_sobol_batch)
-        noises.append(i_noise_batch)
+        i_sobols.append(i_sobol_batch)
+        i_noises.append(i_noise_batch)
+        i_examples.append(i_example_batch)
 
     # sort according to the sobol index
-    sorted_indices = tf.argsort(tf.concat(sobols, axis=0), axis=0)
+    sorted_indices = tf.argsort(tf.concat(i_sobols, axis=0), axis=0)
 
     # shape (n_cosmos, n_examples_per_cosmo, None)
-    preds = stack_grid_cosmos(preds, sorted_indices, n_examples_per_cosmo).numpy()
-    cosmos = stack_grid_cosmos(cosmos, sorted_indices, n_examples_per_cosmo).numpy()
-    sobols = stack_grid_cosmos(sobols, sorted_indices, n_examples_per_cosmo).numpy()
-    noises = stack_grid_cosmos(noises, sorted_indices, n_examples_per_cosmo).numpy()
+    preds = _stack_grid_cosmos(preds, sorted_indices, n_examples_per_cosmo).numpy()
+    cosmos = _stack_grid_cosmos(cosmos, sorted_indices, n_examples_per_cosmo).numpy()
+    i_sobols = _stack_grid_cosmos(i_sobols, sorted_indices, n_examples_per_cosmo).numpy()
+    i_noises = _stack_grid_cosmos(i_noises, sorted_indices, n_examples_per_cosmo).numpy()
+    i_examples = _stack_grid_cosmos(i_examples, sorted_indices, n_examples_per_cosmo).numpy()
     LOGGER.info(f"Reshaped the results")
 
     # double check that the cosmologies are sorted correctly and remove the redundant axis
-    cosmos = remove_example_axis(cosmos)
-    sobols = remove_example_axis(sobols)
-    noises = remove_example_axis(noises)
+    cosmos = _remove_example_axis(cosmos)
+    i_sobols = _remove_example_axis(i_sobols)
+    i_noises = _remove_example_axis(i_noises)
+    i_examples = _remove_example_axis(i_examples)
 
     out_file = _get_out_file(dir_out, file_label)
     with h5py.File(out_file, "a") as f:
         f.create_dataset(name="grid/pred", data=preds)
         f.create_dataset(name="grid/cosmo", data=cosmos)
-        f.create_dataset(name="grid/i_sobol", data=sobols)
-        f.create_dataset(name="grid/i_noise", data=noises)
+        f.create_dataset(name="grid/i_sobol", data=i_sobols)
+        f.create_dataset(name="grid/i_noise", data=i_noises)
+        f.create_dataset(name="grid/i_example", data=i_examples)
 
     LOGGER.info(f"Evaluation of the grid has finished, saved the predictions in {out_file}")
 
 
 def evaluate_fiducial(
-    model, strategy, tfr_pattern, msfm_conf, dlss_conf, net_conf, dir_out, file_label=None, training_set=True
+    model,
+    strategy,
+    tfr_pattern,
+    msfm_conf,
+    dlss_conf,
+    net_conf,
+    dir_out,
+    file_label=None,
+    training_set=True,
 ):
     """Evaluate the model on the fiducial part of the CosmoGrid.
 
-        Args:
-            model (DeltaLossModel): Model to be evaluated.
-            strategy (tf.distribute.Strategy): Distribution strategy instance within which the model has been created. This
-                is used to distribute the dataset.
-            tfr_pattern (str): Glob pattern of the .tfrecord files containing the data.
-            msfm_conf (dict): Configuration file of the msfm pipeline.
-            net_conf (dict): Configuration file of the specific model.
-            dir_out (str): Output directory, this is where the evaluations will be saved.
-            file_label (str, optional): Optional suffix to append to the output file names. Defaults to None.
-            training_set (bool, optional): Whether it's a training or validation set. This changes how the result is stored.
-    I"""
+    Args:
+        model (DeltaLossModel): Model to be evaluated.
+        strategy (tf.distribute.Strategy): Distribution strategy instance within which the model has been created.
+        This is used to distribute the dataset.
+        tfr_pattern (str): Glob pattern of the .tfrecord files containing the data.
+        msfm_conf (dict): Configuration file of the msfm pipeline.
+        net_conf (dict): Configuration file of the specific model.
+        dir_out (str): Output directory, this is where the evaluations will be saved.
+        i_noise (int, str): Noise index. The string "all" is also allowed, then the multi noise dataset is used.
+        file_label (str, optional): Optional suffix to append to the output file names. Defaults to None.
+        training_set (bool, optional): Whether it's a training or validation set. This changes how the result is
+            stored.
+    """
     print("\n")
     LOGGER.info(f"Starting evaluation of the fiducial")
+
+    dset_kwargs = net_conf["dset"]["eval"]["fiducial"]
+
+    if "i_noise" in dset_kwargs.keys():
+        is_single_noise = True
+    elif "n_noise" in dset_kwargs.keys():
+        is_single_noise = False
+    else:
+        raise NotImplementedError(
+            f"The network config needs to contain either i_noise or n_noise in dset/eval/fiducial"
+        )
 
     # pipeline constants
     n_cosmos = 1  # only the true fiducial
     n_patches = msfm_conf["analysis"]["n_patches"]
     n_perms_per_cosmo = msfm_conf["analysis"]["fiducial"]["n_perms_per_cosmo"]
     n_examples_per_cosmo = n_patches * n_perms_per_cosmo
+
+    # multiple shape and poisson noise realizations
+    if not is_single_noise:
+        n_examples_per_cosmo *= msfm_conf["analysis"]["grid"]["n_noise_per_example"]
+
     n_examples = n_cosmos * n_examples_per_cosmo
     LOGGER.info(f"There's a total of {n_examples} data vectors to be evaluated")
 
@@ -224,18 +284,29 @@ def evaluate_fiducial(
 
     # like https://www.tensorflow.org/tutorials/distribute/input#tfdistributestrategydistribute_datasets_from_function
     def dataset_fn(input_context):
-        dset = fiducial_pipeline.get_dset(
-            tfr_pattern=tfr_pattern,
-            **net_conf["dset"]["eval"]["fiducial"],
-            # distribution
-            input_context=input_context,
-        )
+        if is_single_noise:
+            dset = fiducial_pipeline.get_dset(
+                tfr_pattern=tfr_pattern,
+                **dset_kwargs,
+                # distribution
+                input_context=input_context,
+            )
+
+        else:
+            dset = fiducial_pipeline.get_multi_noise_dset(
+                tfr_pattern=tfr_pattern,
+                **dset_kwargs,
+                # distribution
+                input_context=input_context,
+            )
+
         return dset
 
     dist_dset = strategy.distribute_datasets_from_function(dataset_fn)
 
     preds = []
     i_examples = []
+    i_noises = []
     for dv_batch, index_batch in LOGGER.progressbar(
         dist_dset, at_level="info", total=n_steps, desc="evaluating at the fiducial"
     ):
@@ -246,18 +317,22 @@ def evaluate_fiducial(
         pred_batch = strategy.gather(pred_batch, axis=0)
         # shape (global_batch_size)
         i_example_batch = strategy.gather(index_batch[0], axis=0)
+        i_noise_batch = strategy.gather(index_batch[1], axis=0)
 
         preds.append(pred_batch)
         i_examples.append(i_example_batch)
+        i_noises.append(i_noise_batch)
 
     preds = tf.concat(preds, axis=0)
     i_examples = tf.concat(i_examples, axis=0)
+    i_noises = tf.concat(i_noises, axis=0)
     LOGGER.info(f"Reshaped the results")
 
     # sort according to the example index
     sorted_indices = tf.argsort(i_examples)
     preds = tf.gather(preds, sorted_indices)
     i_examples = tf.gather(i_examples, sorted_indices)
+    i_noises = tf.gather(i_noises, sorted_indices)
     LOGGER.info(f"Sorted the results")
 
     out_file = _get_out_file(dir_out, file_label)
@@ -265,8 +340,10 @@ def evaluate_fiducial(
         if training_set:
             f.create_dataset(name="fiducial/train/pred", data=preds)
             f.create_dataset(name="fiducial/train/i_example", data=i_examples)
+            f.create_dataset(name="fiducial/train/i_noise", data=i_noises)
         else:
             f.create_dataset(name="fiducial/vali/pred", data=preds)
             f.create_dataset(name="fiducial/vali/i_example", data=i_examples)
+            f.create_dataset(name="fiducial/vali/i_noise", data=i_noises)
 
     LOGGER.info(f"Evaluation of the fiducial has finished, saved the predictions in {out_file}")
