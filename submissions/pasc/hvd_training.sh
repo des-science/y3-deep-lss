bab0e6db6154e66bb8def169b16d7a22c7f19f17
Arne Thomsen <athomsen@student.ethz.ch>
2024-03-15 06:19:41 -0700

    pasc configs and submissions update

--- submissions/pasc/hvd_training.sh
+++ submissions/pasc/hvd_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=des_g
 #SBATCH --constraint=gpu
-#SBATCH --qos=debug
-#SBATCH --time=00:30:00
+#SBATCH --qos=regular
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
@@ -11,12 +11,13 @@
 #SBATCH --job-name=hvd_training
 #SBATCH --output="./logs/training_%j.log"
 
-export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=INFO
 
 STRATEGY="horovod"
 VERSION="v7"
 # lensing, clustering, combined
 PROBE="combined"
+# PROBE="lensing"
 # linear_bias, quadratic_bias
 BIAS="linear_bias"
 # delta, likelihood
@@ -39,11 +40,11 @@ srun --cpu-bind=threads --gpu-bind=single:1 --output="$OUTPUT" \
     --fidu_vali_tfr_pattern="/pscratch/sd/a/athomsen/DESY3/$VERSION/$BIAS/tfrecords/fiducial/validation/DESy3_fiducial_????.tfrecord" \
     --grid_vali_tfr_pattern="/pscratch/sd/a/athomsen/DESY3/$VERSION/$BIAS/tfrecords/grid/DESy3_grid_????.tfrecord" \
     --dir_base="/pscratch/sd/a/athomsen/run_files/$VERSION/$PROBE/$LOSS" \
-    --dlss_config="configs/$VERSION/pasc/dlss_config.yaml" \
+    --dlss_config="configs/$VERSION/pasc/$PROBE/dlss_config.yaml" \
     --net_config="configs/$VERSION/pasc/resnet_hvd.yaml" \
     --msfm_config="/global/homes/a/athomsen/multiprobe-simulation-forward-model/configs/$VERSION/$BIAS.yaml" \
     --slurm_output="$OUTPUT" \
     --wandb \
-    --wandb_tags "$VERSION" "$PROBE" "$LOSS" "$STRATEGY" "$BIAS" "pasc" "hvd"
+    --wandb_tags "$VERSION" "$PROBE" "$LOSS" "$STRATEGY" "$BIAS" "pasc"
     # --wandb_notes="Run with dropout, the new and improved validation tracking and reduced jacobian weight"
 
--- submissions/pasc/tf_training.sh
+++ submissions/pasc/tf_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=des_g
 #SBATCH --constraint=gpu
-#SBATCH --qos=debug
-#SBATCH --time=00:30:00
+#SBATCH --qos=regular
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=4
@@ -15,6 +15,7 @@ STRATEGY="mirrored"
 VERSION="v7"
 # lensing, clustering, combined
 PROBE="combined"
+# PROBE="lensing"
 # linear_bias, quadratic_bias
 BIAS="linear_bias"
 # delta, likelihood
@@ -37,10 +38,10 @@ srun --cpu-bind=threads --gpu-bind=none --output="$OUTPUT" \
     --fidu_vali_tfr_pattern="/pscratch/sd/a/athomsen/DESY3/$VERSION/$BIAS/tfrecords/fiducial/validation/DESy3_fiducial_????.tfrecord" \
     --grid_vali_tfr_pattern="/pscratch/sd/a/athomsen/DESY3/$VERSION/$BIAS/tfrecords/grid/DESy3_grid_????.tfrecord" \
     --dir_base="/pscratch/sd/a/athomsen/run_files/$VERSION/$PROBE/$LOSS" \
-    --dlss_config="configs/$VERSION/pasc/dlss_config.yaml" \
+    --dlss_config="configs/$VERSION/pasc/$PROBE/dlss_config.yaml" \
     --net_config="configs/$VERSION/pasc/resnet_tf.yaml" \
     --msfm_config="/global/homes/a/athomsen/multiprobe-simulation-forward-model/configs/$VERSION/$BIAS.yaml" \
     --slurm_output="$OUTPUT" \
     --wandb \
-    --wandb_tags "$VERSION" "$PROBE" "$LOSS" "$STRATEGY" "$BIAS" "pasc" "tf"
+    --wandb_tags "$VERSION" "$PROBE" "$LOSS" "$STRATEGY" "$BIAS" "pasc"
     # --wandb_notes="Run with dropout, the new and improved validation tracking and reduced jacobian weight"
